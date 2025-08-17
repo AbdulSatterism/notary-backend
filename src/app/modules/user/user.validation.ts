dfdca4d862912040593aeb35b7b76f88155180d9
AbdulSatterism <abdulsatter.ism@gmail.com>
2025-08-17 17:11:15 +0600

    update user validation

--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -4,7 +4,6 @@ const createUserSchema = z.object({
   body: z.object({
     name: z.string().min(1, 'Name is required'),
     email: z.string().email('Invalid email address'),
-    phone: z.string(),
     password: z.string().min(8, 'Password must have at least 8 characters'),
   }),
 });
@@ -16,13 +15,6 @@ const updateUserProfileSchema = z.object({
   body: z.object({
     name: z.string().optional(),
     phone: z.string().optional(),
-    country: z.string().optional(),
-    injury: z.string().optional(),
-    age: z.number().optional(),
-    height: z.number().optional(),
-    weight: z.number().optional(),
-    gender: z.enum(['MALE', 'FEMALE', 'OTHERS']).optional(),
-    fitnessLevel: z.enum(['BASIC', 'INTERMEDIATE', 'ADVANCED']).optional(),
   }),
 });
 
